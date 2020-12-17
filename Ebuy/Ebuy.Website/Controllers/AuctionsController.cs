a430ece4e080adf89e9c03e2dc3069aec36b4193
Thx <Forethic@outlook.com>
2020-12-17 21:56:43 +0800

    Ebuy -  新增 Controler + 新增 View/Details

---
+++ Ebuy/Ebuy.Website/Controllers/AuctionsController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ebuy.Website.Models;
+
+namespace Ebuy.Website.Controllers
+{
+    public class AuctionsController : Controller
+    {
+        //
+        // GET: /Auctions/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(long id = 0)
+        {
+            var auction = new Auction
+            {
+                Id = id,
+                Title = "Brand new Widget 2.0",
+                Description = "This is a brand new version 2.0 Widget!",
+                StartPrice = 1.00m,
+                CurrentPrice = 13.40m,
+                StartTime = DateTime.Parse("6-15-2012 12:34 PM"),
+                EndTime = DateTime.Parse("6-23-2012 12:34 PM")
+            };
+
+            return View(auction);
+        }
+
+        public ActionResult Create() { return View(); }
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                return View();                
+            }
+        }
+
+        public ActionResult Edit(int id) { return View(); }
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id) { return View(); }
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                return View();
+            }
+        }
+    }
+}
